7c0ff20a83f6d2bfb3f51f1bf063c48eece35d5e
KaranBhut44 <70577660+KaranBhut44@users.noreply.github.com>
2022-03-29 00:10:21 +0530

    Create Course_Schedule_II.cpp

---
+++ Graph/Course_Schedule_II.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int>s;
+    bool DFS(int index,bool *v1,bool *v2, vector<int> *adj)
+    {
+        v1[index] = true;
+        v2[index] = true;
+        for(auto nbr:adj[index])
+        {
+            if(v2[nbr] == true)
+                return true;
+
+            if(v1[nbr] == false && DFS(nbr,v1,v2,adj))
+                return true;
+        }
+        v2[index] = false;
+        s.push_back(index);
+        return false;
+    }
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        bool visited[V], samePath[V];
+        memset(visited,false,sizeof(visited));
+        memset(samePath,false,sizeof(samePath));
+        
+        for(int i=0;i<V;i++)
+        {
+            if(visited[i] == false)
+            {
+                if(DFS(i,visited,samePath,adj))
+                    return true;
+
+                memset(samePath, false, sizeof(samePath));
+            }
+        }
+        return false;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        for(auto edge:prerequisites)
+        {
+            adj[edge[1]].push_back(edge[0]);
+        }
+        
+        if(isCyclic(numCourses,adj))
+            return {};
+        reverse(s.begin(),s.end());
+        return s;
+    }
+};
