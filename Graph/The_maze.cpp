c9801535fba809ae595fef62ac3e7fc5d7a44315
KaranBhut44 <70577660+KaranBhut44@users.noreply.github.com>
2022-03-28 22:02:02 +0530

    Added non-optimized solution of the maze

--- Graph/The_maze.cpp
+++ Graph/The_maze.cpp
@@ -8,8 +8,12 @@ public:
      */
     bool hasPath(vector<vector<int>> &maze, vector<int> &start, vector<int> &destination) {
         // write your code here
+        int x1 = start[0];
+        int y1 = start[1];
+        int x2 = destination[0];
+        int y2 = destination[1];
         queue<pair<int,int>>q;
-        q.push({start[0],start[1]});
+        q.push({x1,y1});
         int n = maze.size();
         int m = maze[0].size();
         bool **visited = new bool*[n];
@@ -21,33 +25,107 @@ public:
                 visited[i][j] = false;
             }
         }
-        visited[start[0]][start[1]] = true;
+        visited[x1][y1] = true;
         while(!q.empty())
         {
             int old_x = q.front().first;
             int old_y = q.front().second;
-            if(old_x == destination[0] && old_y == destination[1])
+            //cout<<old_x<<" "<<old_y<<endl;
+            if(old_x == x2 && old_y == y2)
                 return true;
             q.pop();
-            int arr[5] = {0,1,0,-1,0};
-            for(int i=0;i<4;i++)
+            //left
+            int x = old_x;
+            int y = old_y;
+            while(maze[x][y] != 1 && y!=0)
             {
-                int x = old_x;
-                int y = old_y;
-                
-                while(x>=0 && y>=0 && x<=n-1 && y<=m-1 && maze[x][y] == 0)
+                y--;
+            }
+            if(maze[x][y] == 1)
+            {
+                y++;
+                if(old_y > y && visited[old_x][y] == false)
                 {
-                    y += arr[i+1];
-                    x += arr[i];
+                    //cout<<old_x<<" "<<old_y<<" left1 "<<old_x<<" "<<y<<endl;
+                    q.push({old_x,y});
+                    visited[old_x][y] = true;
                 }
-                y -= arr[i+1];
-                x -= arr[i];
-                if(visited[x][y] == false)
+            }
+            else if(visited[old_x][y] == false)
+            {
+                //cout<<old_x<<" "<<old_y<<" left2 "<<old_x<<" "<<y<<endl;
+                visited[old_x][y] = true;
+                q.push({old_x,y});
+            }
+            //top
+            x = old_x;
+            y = old_y;
+            while(maze[x][y] != 1 && x!=0)
+            {
+                x--;
+            }
+            if(maze[x][y] == 1)
+            {
+                x++;
+                if(old_x > x && visited[x][old_y] == false)
                 {
-                    q.push({x,y});
-                    visited[x][y] = true;
+                    //cout<<old_x<<" "<<old_y<<" top1 "<<x<<" "<<old_y<<endl;
+                    q.push({x,old_y});
+                    visited[x][old_y] = true;
                 }
-            }  
+            }
+            else if(visited[x][old_y] == false)
+            {
+                //cout<<old_x<<" "<<old_y<<" top2 "<<x<<" "<<old_y<<endl;
+                visited[x][old_y] = true;
+                q.push({x,old_y});
+            }
+            //right
+            x = old_x;
+            y = old_y;
+            while(maze[x][y] != 1 && y!=m-1)
+            {
+                y++;
+            }
+            if(maze[x][y] == 1)
+            {
+                y--;
+                if(old_y < y && visited[old_x][y] == false)
+                {
+                    //cout<<old_x<<" "<<old_y<<" right1 "<<old_x<<" "<<y<<endl;
+                    q.push({old_x,y});
+                    visited[old_x][y] = true;
+                }
+            }
+            else if(visited[old_x][y] == false)
+            {
+                //cout<<old_x<<" "<<old_y<<" right2 "<<old_x<<" "<<y<<endl;
+                visited[old_x][y] = true;
+                q.push({old_x,y});
+            }
+            //bottom
+            x = old_x;
+            y = old_y;
+            while(maze[x][y] != 1 && x!=n-1)
+            {
+                x++;
+            }
+            if(maze[x][y] == 1)
+            {
+                x--;
+                if(old_x < x && visited[x][old_y] == false)
+                {
+                    //cout<<old_x<<" "<<old_y<<" bottom1 "<<x<<" "<<y<<endl;
+                    q.push({x,old_y});
+                    visited[x][old_y] = true;
+                }
+            }
+            else if(visited[x][old_y] == false)
+            {
+                //cout<<old_x<<" "<<old_y<<" bottom2 "<<x<<" "<<old_y<<endl;
+                visited[x][old_y] = true;
+                q.push({x,old_y});
+            }
         }
         return false;
     }
