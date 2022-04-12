abecb56fd3c8759758b90b4ca7e83408f2203cbe
KaranBhut44 <70577660+KaranBhut44@users.noreply.github.com>
2022-04-12 16:07:52 +0530

    Modularized code

--- Dynamic Programming/House_robber_II.cpp
+++ Dynamic Programming/House_robber_II.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int ans1 = 0,ans2 = 0;
-        int n = nums.size();
-        if(n == 1)
-            return nums[0];
-        //exclude_last 
-        int last = nums[n-2];
+    int find_max(vector<int> &nums,int n)
+    {
+        int last = nums[n-1];
         int second_last = last;
-        for(int i=n-3;i>=0;i--)
-        {
-            int dontpick = second_last;
-            int pick = nums[i];
-            if(i+2 < n-1)
-                pick += last;
-            last = second_last;
-            second_last = max(pick,dontpick);
-        }
-        ans1 = second_last;
-        //exclude first
-        last = nums[n-1];
-        second_last = last;
-        for(int i=n-2;i>0;i--)
+        for(int i=n-2;i>=0;i--)
         {
             int dontpick = second_last;
             int pick = nums[i];
@@ -30,7 +13,24 @@ public:
             last = second_last;
             second_last = max(pick,dontpick);
         }
-        ans2 = second_last;
+        return second_last;
+    }
+    int rob(vector<int>& nums) {
+        int ans1 = 0,ans2 = 0;
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
+        
+        vector<int>temp1,temp2;
+        for(int i=0;i<n;i++)
+        {
+            if(i!=0) temp2.push_back(nums[i]);
+            if(i!=n-1) temp1.push_back(nums[i]);
+        }
+        //exclude_last
+        ans1 = find_max(temp1,n-1);
+        //exclude first
+        ans2 = find_max(temp2,n-1);
         //find_max
         return max(ans1,ans2);
     }
